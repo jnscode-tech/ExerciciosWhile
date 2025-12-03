004e4317770633bfe7c421b4f80ad4c9ad62ac03
jnscode-tech <juliana_jns_2010@hotmail.com>
2025-12-03 20:13:56 -0300

    Correção do exercício 8. fórmula num %2 != 2 para falar que o número é ímpar.

--- exercicio08.php
+++ exercicio08.php
@@ -4,12 +4,12 @@
 /* 8 - Crie um programa que utilize while para exibir todos os números ímpares no intervalo de 1 a 15. 
 O programa deve verificar se o número atual é ímpar antes de mostrá-lo. */
 
-$contador = 0;
+$contador = 1;
 
 echo"<strong> Números ímpares no intervalo de 01 a 15. </strong>"."<br>"."<br>";
 
 while($contador <=15){
-     if($contador%3==0){
+     if($contador%2!=0){
          echo "Número ímpar".":". $contador."<br>";
 
      }
